70725bd092cbfb5304a923039c59d4a89b7d3298
grzesiekt15 <tymoczko69@gmail.com>
2022-11-13 19:24:57 +0100

    Tworzenie tablicy statycznej oraz wypisywanie jej elementow

--- main.c
+++ main.c
@@ -3,6 +3,22 @@
 
 int main()
 {
-    printf("Hello world!\n");
+	int i, k;
+    int tablica[3][3] =
+    {
+        {3,5,8},
+        {4,2,1}, //przygotowanie tablicy statycznej
+        {6,9,7},
+    };
+    //petla wypisuje elementy tablicy
+    for(i = 0;  i<3; i++)
+    {
+    	for(k=i; k<3; k++)
+    	{
+    		printf("W[%d]K[%d]: %d, ",i,k, tablica[i][k]);
+		}
+	}
+
+
     return 0;
 }
